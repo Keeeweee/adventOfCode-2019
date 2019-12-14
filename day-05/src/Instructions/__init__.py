ea7c416823bd71c7677de622fcda1943a5f90a50
epsilonmajorquezero <epsilonmajorquezero@gmail.com>
2019-12-14 19:15:50 +0100

    Fix __init__

--- day-05/src/Instructions/__init__.py
+++ day-05/src/Instructions/__init__.py
@@ -2,3 +2,7 @@ from .Sum import Sum
 from .Mult import Mult
 from .Input import Input
 from .Output import Output
+from .Equals import Equals
+from .JumpIfFalse import JumpIfFalse
+from .JumpIfTrue import JumpIfTrue
+from .LessThan import LessThan
