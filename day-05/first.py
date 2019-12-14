8d5ba75e8074143d7113c232a345d19633ecf6fb
epsilonmajorquezero <epsilonmajorquezero@gmail.com>
2019-12-14 18:34:56 +0100

    Fix missing namespace call

--- day-05/first.py
+++ day-05/first.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     with open('data/data.txt') as data:
         code = [int(x) for x in data.readline().split(',')]
 
-    parser = Parser(code)
+    parser = Parser.Parser(code)
 
     parser.run()
 
